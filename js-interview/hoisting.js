857d1c4d2a4d5c2ea5722790417f7dd26f8fe6e5
sanketpm7 <sanket.muttin@patientry.ai>
2025-12-16 23:58:38 +0530

    archieve the exisiting progress

--- js-interview/hoisting.js
+++
@@ -1,4 +0,0 @@
-console.log(x);
-let x = 10;
-
-console.log(x);
--- js-interview/object_creation.js
+++
@@ -1,9 +0,0 @@
-// way 1
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-}
-const p1 = new Person('Sanket');
-console.log(p1)
-
